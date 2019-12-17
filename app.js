ce7f4bd4e8e92ba83d670e5cb115fedd0fd7ba05
lloydnoone <lloydnoone92@gmail.com>
2019-12-17 20:31:10 +0000

    push to github

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-app.set('view engine', 'ejs');
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`))
 
-app.get('/', function(req, res){
-    res.render('home');
-});
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/views/home.html`)
+})
 
 app.listen(process.env.PORT || 27017, function(){
-    console.log("succesfull...");
-});
-
-// local host
-
-// app.listen(3000, function(){
-//     console.log('YelpCamp server has started. ');
-// });
+  console.log('succesfull...')
+})
