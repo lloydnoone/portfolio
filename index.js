4b12455fb7f6e81f2f185ecb42cfa772c3151e49
lloydnoone <lloydnoone92@gmail.com>
2019-12-20 08:39:33 +0000

    deploy with webpack

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express()
 app.use(express.static(`${__dirname}/public`))
 
 app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/views/home.html`)
+  res.sendFile(`${__dirname}/dist/index.html`)
 })
 
 app.listen(process.env.PORT || 27017, function(){
